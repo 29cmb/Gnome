307e9ef3d0c268d7537771122d9e6d665c5f2ebf
29cmb <devcmb@outlook.com>
2026-06-21 14:38:03 -0400

    fix: :bug: Fix spirits always counting as normal (i think)

--- src/main/kotlin/xyz/devcmb/gnome/data/SpiritTier.kt
+++ src/main/kotlin/xyz/devcmb/gnome/data/SpiritTier.kt
@@ -3,7 +3,8 @@ package xyz.devcmb.gnome.data
 import net.minecraft.resources.Identifier
 
 enum class SpiritTier(val match: String, val resource: Identifier) {
-    NORMAL("", Identifier.fromNamespaceAndPath("mcc", "island_items/infinibag/material/spirit_strong")),
+    // Normal doesn't have a prefix, so it should only be used when the other 2 fail to match
+    NORMAL("According to all known laws of aviation", Identifier.fromNamespaceAndPath("mcc", "island_items/infinibag/material/spirit_strong")),
     REFINED("Refined", Identifier.fromNamespaceAndPath("mcc", "island_items/infinibag/material/spirit_refined_strong")),
     PURE("Pure", Identifier.fromNamespaceAndPath("mcc", "island_items/infinibag/material/spirit_pure_strong")),
 }
